deb463e8037c742be4a69e08960081f854c1cf25
crutchm <sads1100@gmail.com>
2021-09-08 22:43:17 +0500

    bootstrap верстка

--- src/main/java/com/example/demoweb/model/post.java
+++ src/main/java/com/example/demoweb/model/post.java
@@ -1,14 +1,20 @@
 package com.example.demoweb.model;
 
-public class post {
+import java.util.Date;
+
+public class post{
     private String text;
     private int likes;
+    private Date creationDate;
 
-    public post(String text, int likes){
+    public post(String text, int likes, Date creationDate){
         this.text = text;
         this.likes = likes;
+        this.creationDate = creationDate;
     }
-
+    public Date getCreationDate(){
+        return creationDate;
+    }
     public String getText(){
         return text;
     }
--- src/main/java/com/example/demoweb/service/PostService.java
+++ src/main/java/com/example/demoweb/service/PostService.java
@@ -4,15 +4,16 @@ import com.example.demoweb.model.post;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 public class PostService {
     public List<post> listAllPosts(){
         ArrayList<post> result = new ArrayList<>();
-        result.add(new post("fskjnvfjwdnmvfodwmvoiw", 0));
-        result.add(new post("kjhsdnckjsdanjvcksamovk", 0));
-        result.add(new post("единственный адекватный текст", 1));
+        result.add(new post("fskjnvfjwdnmvfodwmvoiw", 0, new Date()));
+        result.add(new post("kjhsdnckjsdanjvcksamovk", 0, new Date()));
+        result.add(new post("единственный адекватный текст", 1, new Date()));
         return result;
     }
 }
